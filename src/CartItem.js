32cb0a79a6354c356564bc098157fd16279458f3
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-04-08 04:50:57 +0530

    added cart component

--- src/CartItem.js
+++ src/CartItem.js
@@ -5,12 +5,63 @@ class CartItem extends React.Component {
         super();
         this.state={
             title:'Phone',
-            Qty:'1',
+            qty:1,
             price:'Rs 999'
         }
+    
+     // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
     }
+
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
+
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     console.log('state', this.state);
+  //   });
+  // }
+  increaseQuantity = () => {
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // }, () => {});
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
+      return;
+    }
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
     render () {
-        const{title,Qty,price}=this.state;
+        const{title,qty,price}=this.state;
       return (
         <div className="cart-item">
           <div className="left-block">
@@ -19,9 +70,26 @@ class CartItem extends React.Component {
           <div className="right-block">
             <div style={ { fontSize: 25 } }>{title}</div>
             <div style={ { color: '#777' } }>{price}</div>
-            <div style={ { color: '#777' } }>Qty:{Qty}</div>
+            <div style={ { color: '#777' } }>Qty:{qty}</div>
             <div className="cart-item-actions">
               {/* Buttons */}
+              <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick={this.decreaseQuantity}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            />
             </div>
           </div>
         </div>
