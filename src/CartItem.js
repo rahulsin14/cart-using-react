971454004c41745d81f68b871d5f722f8db0df67
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-04-08 03:42:12 +0530

    added constructor

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            title:'Phone',
+            Qty:'1',
+            price:'Rs 999'
+        }
+    }
     render () {
+        const{title,Qty,price}=this.state;
       return (
         <div className="cart-item">
           <div className="left-block">
             <img style={styles.image} />
           </div>
           <div className="right-block">
-            <div style={ { fontSize: 25 } }>Phone</div>
-            <div style={ { color: '#777' } }>Rs 999</div>
-            <div style={ { color: '#777' } }>Qty: 1</div>
+            <div style={ { fontSize: 25 } }>{title}</div>
+            <div style={ { color: '#777' } }>{price}</div>
+            <div style={ { color: '#777' } }>Qty:{Qty}</div>
             <div className="cart-item-actions">
               {/* Buttons */}
             </div>
