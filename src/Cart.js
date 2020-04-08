930a1236f5ca3d13c146e70d0fdeecbfdf4c65be
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-04-08 16:54:14 +0530

    added delete functionality in cartItems

--- src/Cart.js
+++ src/Cart.js
@@ -32,12 +32,42 @@ class Cart extends React.Component {
      // this.increaseQuantity = this.increaseQuantity.bind(this);
     // this.testing();
     }
+    handleIncreaseQuantity=(product)=>{
+        console.log(product);
+        const{products}=this.state;
+        const index=products.indexOf(product);
+        products[index].qty+=1;
+        this.setState({
+            products:products
+        })
+    }
+    handleDecreaseQuantity=(product)=>{
+        console.log(product);
+        const{products}=this.state;
+        const index=products.indexOf(product);
+        if(products[index].qty!==0){
+            products[index].qty-=1;
+        }
+        this.setState({
+            products:products
+        })
+    }
+    handleDeleteProduct=(id)=>{
+        const{products}=this.state;
+        const items=products.filter((item)=>item.id!==id);
+        this.setState({
+            products:items
+        })
+    }
     render(){
         const{products}=this.state;
         return(
             <div className="cart">
                 {products.map((product)=>{
-                    return <CartItem product={product} key={product.id} />
+                    return <CartItem product={product} key={product.id} 
+                            onIncreaseQuantity={this.handleIncreaseQuantity} 
+                            onDecreaseQuantity={this.handleDecreaseQuantity}
+                            onDeleteProduct={this.handleDeleteProduct} />
                 })}
             </div>
             
